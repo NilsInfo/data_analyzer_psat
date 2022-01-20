baa0320fa07e3b8e68962d6e9af1fba9fa2071b6
timothe.berthier@insa-lyon.fr <berthiertimothe@gmail.com>
2022-01-21 00:13:31 +0100

    plots the mean std deviation and the std deviations for each % consumed

--- main.py
+++ main.py
@@ -21,26 +21,69 @@ def get_data(fileName):
             if "id" in l: # checks if the syntax of the line is correct
                 jsonl = json.loads(l)
                 if jsonl["id"] in labels.keys(): # checks if it's a recorded id
-                    if jsonl["id"] == "tr60_wifi4_lum0":
-                        print("yo")
+                    # if jsonl["id"] == "tr60_wifi4_lum0":
+                    #     print("yo")
                     if jsonl["level"] != 1 : # We only take data when the battery reaches 99%
                         if len(data[labels[jsonl["id"]]][0]) == 0 : # add an offset for the timestamps to be relative
                             offsets[jsonl["id"]] = int(jsonl["ts"])
 
                         data[labels[jsonl["id"]]][0].append(int(jsonl["ts"]) - offsets[jsonl["id"]])
-                        data[labels[jsonl["id"]]][1].append(jsonl["level"]*100)
-    print(realNames)
-    print(labels)
-    print(data)
+                        data[labels[jsonl["id"]]][1].append(99-jsonl["level"]*100)
+    # print(realNames)
+    # print(labels)
+    # print(data[0])
     minsTimesElapsed = []
     # calculate the experience with the minimum of time elapsed
     for i in range(len(data)):
-        print(i)
-        print(data[i])
+        # print(i)
+        # print(data[i])
         minsTimesElapsed.append(data[i][0][-1] - data[i][0][0])
     minTime = min(minsTimesElapsed)
-    print(minsTimesElapsed)
+    # print(minsTimesElapsed)
     return realNames, data, minTime
+
+#returns the array with the percentage transition time, from 0% to 8% (of battery consumption)
+def get_levels_array(data):
+    totalLevels = []
+    # print(data[0][0])
+    for i in range(len(data)):
+        expeLevels = [[],[]]
+        for j in range(1,len(data[i][1])):
+            if((data[i][1][j-1]!=data[i][1][j]) and data[i][1][j-1]<8):         #if percentage transition and percentage < 8
+                expeLevels[1].append(data[i][1][j])         #appending the percentage value
+                expeLevels[0].append(data[i][0][j]-10)      #appending the corresponding time, -10 to get the middle of the interval of time
+        totalLevels.append(expeLevels)
+    # print(totalLevels)
+    return totalLevels
+
+def get_average_levels(data):
+    average_levels = [[],[]]
+    for i in range(len(data[0][0])):
+        sum=0
+        for j in range(len(data)):
+            sum+=data[j][0][i]
+            average=sum/len(data)
+        average_levels[0].append(average)
+        average_levels[1].append(i+1)
+
+    return average_levels
+
+def get_standard_deviation(levels_array,average_levels):
+    standard_deviations = [[],[]]
+
+    for i in range(len(average_levels[0])):
+
+        mean_value=average_levels[0][i]
+        sum=0
+
+        for j in range(len(levels_array)):
+            sum+=(mean_value-levels_array[j][0][i])**2
+
+        standard_deviations[0].append(np.sqrt(sum/len(levels_array)))
+        standard_deviations[1].append(i+1)
+
+    return standard_deviations , np.mean(standard_deviations[0])
+
 def plot_bar_chart(realNames, data, minTime):
     labels = ['Allociné', 'Le parisien', '20 minutes', 'Marmiton']
     web_with_ads = []
@@ -90,17 +133,39 @@ def plot_data(realNames, data, minTime):
             minLen = len(data[i][0] )- 1
         ax.plot(data[i][0][:minLen],data[i][1][:minLen], label = realNames[data[i][2]])
         minsLevels.append(min(data[i][1][:minLen])) # find the minimum level reached within the min time of exp
+
     # clean it
-    ax.set_yticks(np.arange(min(minsLevels)-1,100, step=1))
+    ax.set_yticks(np.arange(0,10, step=1))
     ax.set_xlabel("time (sec)")
-    ax.set_ylabel("battery (%)")
+    ax.set_ylabel("battery consumed (%)")
+    ax.grid(which = "major")
+    plt.legend(loc="upper right", frameon=False)
+    plt.show()
+
+
+def display_standard_deviations(standard_deviations, mean):
+    fig, ax = plt.subplots(1, figsize=(8, 6))
+
+    ax.bar(standard_deviations[1],standard_deviations[0], label = "standard deviation")
+    ax.axhline(y=mean, color='r', linestyle='-', label= "mean standard deviation")
+    # ax.set_xticks(np.arange(0,11, step=1))
+    ax.set_yticks(np.arange(0,200, step=25))
+    ax.set_ylabel("standard deviation of time measured (sec)")
+    ax.set_xlabel("battery consumed (%)")
     ax.grid(which = "major")
     plt.legend(loc="upper right", frameon=False)
     plt.show()
 
 if __name__ == '__main__':
-    realNames, data, minTime = get_data("adblock.txt")
+    realNames, data, minTime = get_data("exp_recurrence.txt")
+    levels_array = get_levels_array(data)
+    # print(levels_array)
+    average_levels = get_average_levels(levels_array)
+    standard_deviations, mean_standard_deviation = get_standard_deviation(levels_array,average_levels)
+    print(standard_deviations)
+    print(mean_standard_deviation)
+    display_standard_deviations(standard_deviations, mean_standard_deviation)
     # plot_data(realNames, data, minTime)
-    plot_bar_chart(realNames, data, minTime)
+    # plot_bar_chart(realNames, data, minTime)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
